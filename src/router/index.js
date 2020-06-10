c7063ffadb83450b95816c976fe73ccfd0d01bc9
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-06-10 17:30:55 +0430

    Configure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import TodoList from '../components/TodoList'
 
 Vue.use(VueRouter)
 
-  const routes = []
+  const routes = [
+    {
+      path: '/completed',
+      component: TodoList,
+      props: {
+        completed: true,
+        isMain: true
+      },
+    },
+    {
+      path: '/list/:id',
+      component: TodoList,
+      props: {
+        isMain: false,
+      },
+    },
+    {
+      path: '/',
+      component: TodoList,
+      props: {
+        isMain: true,
+      },
+    },
+  ]
 
 const router = new VueRouter({
   routes
