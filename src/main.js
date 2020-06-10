efe35f8f1dcc76aa1c5e5f19dc536221801a6399
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-06-10 17:29:28 +0430

    Setup base url and vuex store

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = process.env.VUE_APP_API
 
 Vue.use(VueMaterial)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
